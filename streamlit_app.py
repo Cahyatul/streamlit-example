2aa45a08edc5c53222cfc391d53bb8a4743754c8
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 11:07:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import pandas as pd
 import streamlit as st
 from transformers import pipeline
 
+st.title('Text_Summary')
+st.title('_Streamlit_ is :blue[cool] :sunglasses:')
 """
 # Peringkas Artikel
 Ringkas Artikelmu Disini
