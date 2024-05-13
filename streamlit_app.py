3e5010414e92b194a1fcfb26e037075f1eae504b
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:39:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title('RingkasID.')
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
 
-txt = st.text_area(
+txt = st.text_area, (
     "")
-
 st.write(f"You wrote {len(txt)} characters.")
+st.form_submit_button(label="Submit", help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
