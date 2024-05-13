051fbbe2995b2c8226d3cc654a18dcd9a7ac9c4f
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 11:45:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.title('RingkasID.')
 """
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
+st.form_submit_button(label="Submit", help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
