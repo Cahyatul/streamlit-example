f0118291b39f2abba0d37f626e8c65607d5dc8e1
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:29:33 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,18 @@ st.title('RingkasID.')
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
 
+txt = st.text_area(
+    "Text to analyze",
+    "It was the best of times, it was the worst of times, it was the age of "
+    "wisdom, it was the age of foolishness, it was the epoch of belief, it "
+    "was the epoch of incredulity, it was the season of Light, it was the "
+    "season of Darkness, it was the spring of hope, it was the winter of "
+    "despair, (...)",
+    )
+
+st.write(f"You wrote {len(txt)} characters.")
+
+
 uploaded_files = st.file_uploader("Upload file disini", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
