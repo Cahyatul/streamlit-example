ca1884b89850253ca7c89f53785cab6b74d490b8
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:32:14 +0700

    Update streamlit_app.py\

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,15 +5,8 @@ st.title('RingkasID.')
 """
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
-
+Copy Teks Disini
 txt = st.text_area(
     "")
 
 st.write(f"You wrote {len(txt)} characters.")
-
-
-uploaded_files = st.file_uploader("Upload file disini", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
