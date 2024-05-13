b2bec8682a8daaaf606535366395b09c00d4d8a6
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:23:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,10 @@ st.title('RingkasID.')
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
 title = st.text_input("Copy Teks Disini", "")
-st.write("The current movie title is", title)
+st.text.area()
+
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
+    bytes_data = uploaded_file.read()
+    st.write("filename:", uploaded_file.name)
+    st.write(bytes_data)
