4ba235a5d2152c8d87ef7b6c0140be23a87001cb
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 11:32:29 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header('RingkasID.', divider='rainbow')
+st.header('Selamat datang di Aplikasi Peringkas Artikel!', divider='rainbow')
 st.title('RingkasID.')
 """
 Solusi Peringkas Artikel Cepat dan Akurat 
