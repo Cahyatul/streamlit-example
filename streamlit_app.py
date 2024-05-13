c50a987f59b93aa942d13363907a1122ee8d6ac8
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:40:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ Solusi Peringkas Artikel Cepat dan Akurat
 txt = st.text_area, (
     "")
 st.write(f"You wrote {len(txt)} characters.")
-st.form_submit_button(label="Submit", help=None, on_click=None, args=None, kwargs=None, *, type="secondary", disabled=False, use_container_width=False)
+st.form_submit_button(label="Submit")
