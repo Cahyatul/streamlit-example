96691293acfae05b18c3f99721056be87672ced3
Alfian_Hidayat <127319918+AlfianHidayat03@users.noreply.github.com>
2024-05-13 12:18:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,6 @@ st.title('RingkasID.')
 """
 Solusi Peringkas Artikel Cepat dan Akurat 
 """
-def summarize_article(article):
-    summary = summarizer(article, max_length=150, min_length=30, do_sample=False)
-    return summary[0]['summary_text']
-def main():
-    st.title("Aplikasi Peringkas Artikel")
+txt = st.text_area()
 
-    # Tampilkan header dan deskripsi
-    st.markdown("## Ringkas Artikel")
-    st.write("Masukkan teks artikel di bawah ini atau unggah file teks (format .txt) untuk peringkas.")
-
-    # Tampilkan kolom untuk input teks atau unggah file
-    input_type = st.radio("Pilih tipe input:", ("Teks", "Unggah File"))
-    
-    if input_type == "Teks":
-        article_text = st.text_area("Masukkan teks artikel di sini:")
-    else:
-        uploaded_file = st.file_uploader("Unggah artikel (format .txt)", type="txt")
-        if uploaded_file is not None:
-            article_text = uploaded_file.getvalue().decode("utf-8")
-
+st.write(f"You wrote {len(txt)} characters.")
